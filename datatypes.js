1d729edcf32e9a0ca7eb80bf644c34f2209bf896
TarunChandragir-ux <tarunchandragiri14@gmail.com>
2024-10-28 17:43:21 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+const accountId = 1234;
+let accountEmail = "tarun@gmail.com";
+var accountPassword = "12345";
+userName;
+
+console.table(accountId,accountEmail,accountPassword);
+
+// Data types
+// primitive datatypes
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+let xyz = null; // datatypes in object
+let occupation; // undefined datatype
+
+
+console.log(typeof age);
