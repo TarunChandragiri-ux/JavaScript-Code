5a76559e516a4831a2ad4332ec6ce6ed1ac2afc9
TarunChandragir-ux <tarunchandragiri14@gmail.com>
2024-10-29 12:03:45 +0530

    type conversions

---
+++ conversionOperation.js
@@ -0,0 +1,39 @@
+let score =undefined;
+
+console.log(typeof (score));
+
+// for numbers
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log( valueInNumber);
+//  below values are the values of ValueInNumber
+// "33" => 33
+// "33abc" = NaN(not a number)
+// true = 1
+// null = 0
+// undefined = 0
+
+
+
+// for boolean
+let isLoggedIn = 1; 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// IIII => false
+// "hitesh" => true
+
+
+// for String
+let someNumber = 23;
+let stringNumber = String(someNumber);
+
+console.log( typeof stringNumber);
+
+
+
